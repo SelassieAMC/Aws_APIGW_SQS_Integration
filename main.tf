7089d5e5669bab34f06085a74597d3c86a5d3fbe
Andres Clavijo <andres.clavijo@coxautoinc.com>
2023-01-04 11:46:20 -0500

    Create api gw endpoints

--- main.tf
+++ main.tf
@@ -108,3 +108,81 @@ resource "aws_api_gateway_rest_api" "api" {
   name        = "my-sqs-api"
   description = "Manage SQS messages"
 }
+
+resource "aws_api_gateway_method" "api" {
+  rest_api_id          = aws_api_gateway_rest_api.api.id
+  resource_id          = aws_api_gateway_rest_api.api.root_resource_id
+  api_key_required     = false
+  http_method          = "POST"
+  authorization        = "NONE"
+}
+
+#forward records into SQS
+resource "aws_api_gateway_integration" "api" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_rest_api.api.root_resource_id
+  http_method             = "POST"
+  type                    = "AWS"
+  integration_http_method = "POST"
+  passthrough_behavior    = "NEVER"
+  credentials             = aws_iam_role.api.arn
+  uri                     = "arn:aws:apigateway:${var.region}:sqs:path/${aws_sqs_queue.queue.name}"
+
+  request_parameters = {
+    "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
+  }
+
+  request_templates = {
+    "application/json" = "Action=SendMessage&MessageBody=$input.body"
+  }
+}
+
+#handler for success responses
+resource "aws_api_gateway_integration_response" "200" {
+  rest_api_id       = aws_api_gateway_rest_api.api.id
+  resource_id       = aws_api_gateway_rest_api.api.root_resource_id
+  http_method       = aws_api_gateway_method.api.http_method
+  status_code       = aws_api_gateway_method_response.200.status_code
+  selection_pattern = "^2[0-9][0-9]" // regex pattern for any 200 message that comes back from SQS
+
+  response_templates = {
+    "application/json" = "{\"message\": \"Message queued!\"}"
+  }
+
+  depends_on = ["aws_api_gateway_integration.api"]
+}
+
+resource "aws_api_gateway_method_response" "200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.api.http_method
+  status_code = 200
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_deployment" "api" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_method.api.id,
+      aws_api_gateway_integration.api.id
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    "aws_api_gateway_integration.api",
+  ]
+}
+
+resource "aws_api_gateway_stage" "main" {
+  deployment_id = aws_api_gateway_deployment.api.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "main"
+}
